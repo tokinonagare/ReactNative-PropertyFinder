a543f9f71d9e590469b765fb5fe1556fc3f563e6
tokinonagare <timemahou@gmail.com>
2015-10-29 13:26:19 +0800

    Navigator initial

--- index.ios.js
+++ index.ios.js
@@ -10,23 +10,20 @@ var {
   StyleSheet,
   Text,
   View,
+  NavigatorIOS
 } = React;
+
+var Component = require('./Component')
 
 var PropertyFinder = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style = {styles.container}
+        initialRoute = {{
+          title: 'Property Finder',
+          component: Component
+        }}/>
     );
   }
 });
@@ -34,20 +31,8 @@ var PropertyFinder = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: 'white'
+  }
 });
 
 AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
