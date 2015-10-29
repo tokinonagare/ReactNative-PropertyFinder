f1224c12022b2dfab14fd4e1efce07ed2ea745cf
tokinonagare <timemahou@gmail.com>
2015-10-29 16:10:17 +0800

    add searchInput & searchButton

--- App/SearchPage.js
+++ App/SearchPage.js
@@ -23,28 +23,71 @@ var SearchPage = React.createClass({
         <Text style = {styles.description}>
         	Search by place-name, posecode or search near your location.
         </Text>
-      </View> 
+        <View style = {styles.searchContainer}>
+          <TextInput
+            style         = {styles.searchInput}
+            placeholder   = 'Search via name or postcode'/>
+          <TouchableHighlight     
+            style         = {styles.searchButton}
+            underlayColor = 'gray'>
+            <Text style   = {styles.searchButtonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight
+          style         = {styles.searchButton}
+          underlayColor = 'gray'>
+          <Text style   = {styles.searchButtonText}>Location</Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    padding:     30,
+    marginTop:   65,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   description: {
-    fontSize: 18,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 20,
+    fontSize:     16,
+    textAlign:   'center',
+    color:       'gray'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems:    'center',
+    alignSelf:     'stretch'
   },
+  searchInput: {
+    flex:         4,
+    height:       32,
+    padding:      4,
+    marginRight:  5,
+    borderWidth:  1,
+    borderRadius: 8,
+    fontSize:     14,
+    borderColor: 'black',
+    color:       'black'
+  },
+  searchButton: { 
+    flex:             1,
+    height:           32,
+    borderWidth:      1,
+    borderRadius:     8,
+    marginBottom:     10,
+    flexDirection:   'row',
+    justifyContent:  'center',
+    alignSelf:       'stretch',
+    borderColor:     'black',
+    backgroundColor: 'black'
+  },
+  searchButtonText: {
+    fontSize:   18,
+    color:     'white',
+    alignSelf: 'center'
+  }
 });
 
 module.exports = SearchPage;
